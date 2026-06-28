4822665cf4d8fa406ad832b2ab637fbd2e148144
Angelica Garcia <angelicagarcia1908@gmail.com>
2026-06-27 20:51:59 -0500

    feat: Agregar dashboard, proyectos y mejoras responsive en materiales

--- frontend/optiobra/src/app/app.routes.ts
+++ frontend/optiobra/src/app/app.routes.ts
@@ -1,17 +1,15 @@
 import { Routes } from '@angular/router';
-// import { Materiales } from './pages/materiales/materiales';
 import { Login } from './pages/login/login';
-
-// export const routes: Routes = [
-// {
-//  path: '',
-//   component: Materiales
-// }
-// ]
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Materiales } from './pages/materiales/materiales';
+import { Proyectos } from './pages/proyectos/proyectos';
 
 export const routes: Routes = [
- {
-   path: '',
-    component: Login
- }
-]
+  { path: '', component: Login },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'materiales', component: Materiales },
+  { path: 'proyectos', component: Proyectos }
+];
+
+
+
